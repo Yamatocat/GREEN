b5605e678578bc0fc7fa38002d5d2f4c4c204b12
Yamatocat <75007799+Yamatocat@users.noreply.github.com>
2022-01-20 11:26:08 +0900

    GREEN GOGO
    GREEN GOGO

--- GREEN/green/lib/main.dart
+++ GREEN/green/lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -52,6 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
   bool A = false;
   String Acolor = "blue";
   Color _color = const Color.fromARGB(0xFF, 0x42, 0xA5, 0xF5);
+
   void _incrementCounter() {
     setState(() {
       _counter++;
